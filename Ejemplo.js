e1abe2d75405509b0d18329faf5239278421f28a
MauricioRojas-S <mauriciorojas.s2012@gmail.com>
2025-03-29 09:30:20 -0500

    JavaScritp Archivo

---
+++ Ejemplo.js
@@ -0,0 +1,26 @@
+console.log(eval(new String("2 + 2")));
+console.log('\n');
+console.log(eval("2 + 2"));
+
+
+var nombre = "Hola Mundo";
+console.log(nombre)
+
+var variable = "nombre";
+
+let nombre2;
+nombre2 = "Camilo";
+
+const apellido = "Roncha";
+
+console.log(variable, nombre2, apellido)
+
+let nombrecompleto2 = "Sebastian Vanegas";
+let nombreCompleto2 = "Camilo Rocha";
+console.log(nombrecompleto2, nombreCompleto2)
+
+let _1654 = "Hola";
+let $gsdsga = " ";
+let a1654 = "Mundo";
+
+console.log(_1654, $gsdsga, a1654);
