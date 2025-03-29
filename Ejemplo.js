8512c34aad445de1521a49e0b7e3e8d58d2e1a79
MauricioRojas-S <mauriciorojas.s2012@gmail.com>
2025-03-29 09:49:12 -0500

    Segundo Commit

--- Ejemplo.js
+++ Ejemplo.js
@@ -24,3 +24,25 @@ let $gsdsga = " ";
 let a1654 = "Mundo";
 
 console.log(_1654, $gsdsga, a1654);
+
+let x, y;
+x = 7;
+y = 14;
+
+var bandera = true;
+
+var simbolo = Symbol("Mi simbolo");
+var z;
+
+console.log(bandera, simbolo, z)
+
+var objeto = {
+    nombre: "Mauricio",
+    apellido: "Rojas",
+    telefono: 3222916128
+}
+var autos = ["BMW", "Audi", "Volvo"];
+var Y = null;
+
+console.log(objeto[nombre])
+console.log(objeto.nombre)
